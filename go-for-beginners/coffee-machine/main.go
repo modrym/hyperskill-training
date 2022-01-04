197b07f2174a1fadd7d88d08b233f2f3cc4c4ebe
Kamil.Kedziak <modrym@gmail.com>
2022-01-04 11:45:36 +0100

    Coffee machine stage 2

--- go-for-beginners/coffee-machine/main.go
+++ go-for-beginners/coffee-machine/main.go
@@ -2,14 +2,24 @@ package main
 
 import "fmt"
 
+const (
+	water       = 200
+	milk        = 50
+	coffeeBeans = 15
+)
+
+func makeCoffee(cups uint) {
+	fmt.Printf("For %d cups of coffee you will need:\n", cups)
+	fmt.Printf("%d ml of water\n", water*cups)
+	fmt.Printf("%d ml of milk\n", milk*cups)
+	fmt.Printf("%d g of coffee beans\n", coffeeBeans*cups)
+}
+
 func main() {
-	fmt.Println(
-		`Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!`,
-	)
+	fmt.Println("Write how many cups of coffee you will need:")
+
+	var cups uint
+	fmt.Scan(&cups)
+
+	makeCoffee(cups)
 }
