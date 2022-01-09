64535da56d7b46d6803b29e5ef7ad617d0f22d93
Kamil.Kedziak <modrym@gmail.com>
2022-01-09 18:25:46 +0100

    Final version of virtual piano

--- frontend-developer/virtual-piano/script.js
+++ frontend-developer/virtual-piano/script.js
@@ -1,14 +1,42 @@
-let supported_keys = ["KeyA",
-    "KeyS",
-    "KeyD",
-    "KeyF",
-    "KeyG",
-    "KeyH",
-    "KeyJ",
+let supported_keys = [
+    // white keys
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    // black keys
+    "W",
+    "E",
+    "T",
+    "Y",
+    "U",
 ];
 
+function animateKey(id) {
+    let keyItem = document.getElementById(id);
+    keyItem.animate([
+        { backgroundColor: "red" },
+        { backgroundColor: keyItem.style.backgroundColor },
+    ], {
+        duration: 500,
+    });
+
+}
+
 document.addEventListener("keydown", function (e) {
-    if(supported_keys.includes(e.code)) {
-        console.log("The '" + e.key.toUpperCase() + "' key is pressed.");
+    let key = e.key.toUpperCase();
+    if(supported_keys.includes(key)) {
+        console.log("The '" + key + "' key is pressed.");
+
+        // start the animation
+        animateKey(e.code);
+
+        // play the sound
+        let src = "sounds/" + key + ".mp3";
+        let sound = new Audio(src);
+        sound.play();
     }
 });
