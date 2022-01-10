9eecd7910be432e0e21e2f3529761153ac6be3cf
Kamil.Kedziak <modrym@gmail.com>
2022-01-11 00:44:48 +0100

    Go Flashcards stage 2

--- go-for-beginners/flashcards/main.go
+++ go-for-beginners/flashcards/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
+func readValue(reader *bufio.Reader) string {
+	line, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal("there was a problem when reading from stdin")
+	}
+
+	return strings.TrimSpace(line)
+}
+
 func main() {
-	fmt.Println("Card:")
-	fmt.Println("cos'(x)")
-	fmt.Println("Definition:")
-	fmt.Println("-sin(x)")
+	reader := bufio.NewReader(os.Stdin)
+
+	readValue(reader) // discard one value
+
+	if readValue(reader) == readValue(reader) {
+		fmt.Println("Your answer is right!")
+	} else {
+		fmt.Println("Your answer is wrong...")
+	}
 }
