a61fea14d9889e011ce5da9120e9ed2a21a6fd33
Kamil.Kedziak <modrym@gmail.com>
2022-01-11 21:59:22 +0100

    Todo list final stage

--- frontend-developer/to-do-list/script.js
+++ frontend-developer/to-do-list/script.js
@@ -1,3 +1,4 @@
+let taskListArray = storageLoadTasks();
 let taskList = document.getElementById('task-list');
 
 document.getElementById('add-task-button').addEventListener('click', function () {
@@ -13,12 +14,13 @@ document.getElementById('add-task-button').addEventListener('click', function ()
 });
 
 window.onload = function () {
-    addTask("Email David");
-    addTask("Create ideal");
-    addTask("Set up A/B test");
+    for(let task in taskListArray) {
+        task = taskListArray[task];
+        addTask(task.text, task.checked, false);
+    }
 }
 
-function addTask(text) {
+function addTask(text, done=false, save=true) {
     let task = document.createElement('li');
 
     let span = document.createElement('span');
@@ -28,6 +30,11 @@ function addTask(text) {
     let chBox = document.createElement('input');
     chBox.type = "checkbox";
     chBox.addEventListener('input', function (e) {
+        console.log(task);
+        console.log(elementIndex(task));
+        taskListArray[elementIndex(task)].checked = e.target.checked;
+        storageSaveTasks();
+
         if(e.target.checked) {
             span.classList.add("task-done");
         } else {
@@ -35,9 +42,17 @@ function addTask(text) {
         }
     });
 
+    if(done) {
+        span.classList.add("task-done");
+        chBox.checked = true;
+    }
+
     let delBtn = document.createElement('button');
     delBtn.className = "delete-btn";
     delBtn.addEventListener("click", function () {
+        taskListArray.splice(elementIndex(task), 1);
+        storageSaveTasks();
+
         task.remove();
     });
 
@@ -46,6 +61,34 @@ function addTask(text) {
     task.appendChild(delBtn);
 
     taskList.appendChild(task);
+
+    if(save) {
+        taskListArray.push({text: text, done: done});
+        storageSaveTasks();
+    }
 }
 
+function elementIndex(element) {
+    let index = -1;
 
+    while(element) {
+        if(element.nodeName.toUpperCase() === 'LI') {
+            ++index;
+        }
+        element = element.previousSibling;
+    }
+
+    return index;
+}
+
+function storageSaveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(taskListArray));
+}
+
+function storageLoadTasks() {
+    return JSON.parse(localStorage.getItem("tasks")) || [];
+}
+
+function storageClearTasks() {
+    localStorage.removeItem("tasks");
+}
