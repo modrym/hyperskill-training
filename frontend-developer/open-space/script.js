add23326691296d5074cf216896c2d50bc1fddcb
Kamil.Kedziak <modrym@gmail.com>
2022-01-15 00:49:22 +0100

    Open space last stage

--- frontend-developer/open-space/script.js
+++ frontend-developer/open-space/script.js
@@ -1,14 +1,14 @@
 const SuperSecretPassword = "TrustNo1";  // you can't see it
 
 window.onload = function () {
-    const controls = [
-        Array.from(document.querySelectorAll('.levers input')),
-        Array.from(document.querySelectorAll('.check-buttons input')),
-        document.querySelector('.launch-button')
-    ].flat();
+    const levers = Array.from(document.querySelectorAll('.levers input'));
+    const boxes = Array.from(document.querySelectorAll('.check-buttons input'));
+    const controls = [boxes, levers].flat();
 
     changeControlAvailability('disable', controls);
 
+    const launchButton = document.querySelector('.launch-button');
+    launchButton.disabled = true;
     const passwordInput = document.querySelector('.password-input');
     const passwordButton = document.querySelector('.ok-button');
 
@@ -24,6 +24,31 @@ window.onload = function () {
             console.log('Wrong password!');
         }
     });
+
+    function inputCheck() {
+        for(const i in boxes) {
+            if(!boxes[i].checked) {
+                return false;
+            }
+        }
+        for(const i in levers) {
+            if(levers[i].value !== '100') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function inputEvent() {
+        launchButton.disabled = !inputCheck();
+    }
+
+    for(const i in controls) {
+        controls[i].onchange = inputEvent;
+    }
+
+    launchButton.addEventListener('click', animation);
 };
 
 function changeControlAvailability(action, controls) {
@@ -32,4 +57,16 @@ function changeControlAvailability(action, controls) {
     for(let i in controls) {
         controls[i].disabled = !action;
     }
+}
+
+function animation() {
+    let ship = document.querySelector('.rocket');
+
+    ship.animate([
+        {transform: "rotate(30deg)"},
+        {transform: "rotate(30deg) translate(0, -100vh)"}
+    ], {
+        duration: 2000
+    });
+    ship.style.transform = "rotate(30deg) translate(0, -100vh)";
 }
